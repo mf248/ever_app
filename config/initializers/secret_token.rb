04ca97809615d2cad8430959b7ac194e716e277d
Michael Fergus <mfergus48@gmail.com>
2014-01-05 20:13:02 -0600

    Improve the README

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,18 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-EverApp::Application.config.secret_key_base = 'eda1aa1c337d7eaa6d8b8e0d08c0a36f92c04ed0627a839f9fdd20bf702a43431dae770e6f5d6221c7c90884e8a4884508cee1fbeeb1fa2c401980ceb62f861f'
+
+def secure_token
+	token_file = Rails.root.join('.secret')
+	if File.exist?(token_file)
+		# Use the existing token.
+		File.read(token_fil).chomp
+	else
+		# Generate a new token and store it in token_files
+		token = SecureRandom.hex(64)
+		File.write(token_file, token)
+		token
+	end
+end
+
+EverApp::Application.config.secret_key_base = secure_token
